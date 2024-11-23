0e4ddbc2b88fea3bb7e5710f68a125a33befb362
Louis Radtke <louis.radtke@tu-dortmund.de>
2024-11-23 22:06:37 +0100

    add backend sln

---
+++ rdpms-backend/src/RDPMS.Core.Main/Model/REST/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace RDPMS.Core.Main.Model.REST;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
