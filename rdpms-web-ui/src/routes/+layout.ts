94c747fa0f7e35a8802770fb40b8d2662bbc51a1
Louis Radtke <louis.radtke@tu-dortmund.de>
2025-10-03 17:03:11 +0200

    fix ssr related issues

---
+++ rdpms-web-ui/src/routes/+layout.ts
@@ -0,0 +1,5 @@
+// disable SSR and prerendering
+// see https://www.reddit.com/r/sveltejs/comments/199dq9t/comment/kiey5ke/?utm_source=share&utm_medium=web2x&context=3
+export const ssr = false;
+export const prerender = false;
+export const trailingSlash = 'always';
